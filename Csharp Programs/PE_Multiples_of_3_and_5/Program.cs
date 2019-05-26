0daf78fc7e1d84657c5f36f69641e68289ef3802
SSchoor163 <steven_schoor@yahoo.com>
2019-05-25 19:12:56 -0500

    6

---
+++ Csharp Programs/PE_Multiples_of_3_and_5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_Multiples_of_3_and_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            for(int x = 0; x<1000; x++)
+            {
+                if(x%3 == 0)
+                { total += x; }
+                else if(x%5 == 0)
+                {
+                    total += x;
+                }
+               
+            }
+            Console.WriteLine(total);
+            Console.Read();
+        }
+    }
+}
---
+++ Csharp Programs/Towers_of_Hanoi/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Towers_of_Hanoi
+{
+    
+    class Program
+    {
+       static Dictionary<char, Stack<int>> towers = new Dictionary<char, Stack<int>>();
+        static void Main(string[] args)
+        {
+            towers.Add('A', new Stack<int> { });
+            for (int x = 4; x == 0; x--)
+            {
+                towers['A'].Push(x);
+            }
+            towers.Add('B', new Stack<int> { });
+            towers.Add('C', new Stack<int> { });
+
+            Console.WriteLine("Towers of Hanoi");
+            Console.Write("Rules: You must get each number in tower A into tower C\n" +
+                          "You may move only one number at a time, and a higher number can not sit atop a lower number.\n");
+
+        }
+        static void printboard()
+        {
+
+        }
+        static bool victoryConditions()
+        {
+            return false;
+        }
+    }
+    
+}
