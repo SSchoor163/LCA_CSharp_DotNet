b5ef80f6b68491a0d87f567ad3fd7f5ce26841c2
sschoor163 <steven_Schoor@yahoo.com>
2019-06-11 21:56:18 -0500

    superheo and supervillian, and Point

---
+++ Csharp Programs/Points/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Points
+{
+    public class Point2D
+    {
+       public int Y { set; get; }
+       public int X { set; get; }
+
+        public Point2D()
+        {
+            X = 0;
+            Y = 0;
+
+        }
+        public Point2D(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType().Equals(this.GetType()))
+            {
+                Point2D temp = (Point2D)obj;
+                if (temp.X == X && temp.Y == Y)
+                    return true;
+                else return false;
+            }
+            else return false;
+        }
+    }
+
+    public class Point3D : Point2D
+    {
+
+        int Z { get; }
+
+        public Point3D()
+        {
+            X = 0;
+            Y = 0;
+            Z = 0;
+
+        }
+        public Point3D(int x, int y, int z) : base(x, y)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public override string ToString()
+        {
+            return $"({X}, {Y}, {Z})";
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType().Equals(this.GetType()))
+            {
+                Point3D temp = (Point3D)obj;
+                if (temp.X == X && temp.Y == Y && temp.Z == Z)
+                    return true;
+                else return false;
+            }
+            else return false;
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point2D p1 = new Point2D(), p2 = new Point2D(7, 5), p3 = new Point2D(-2, 4), p4 = new Point2D(7, 5);
+
+            Console.WriteLine($"p1 = {p1}");
+            Console.WriteLine($"p2 = {p2}");
+            Console.WriteLine($"p3 = {p3}");
+            Console.WriteLine($"p4 = {p4}");
+
+            Console.WriteLine($"p2 == p3? {p2==p3}");
+            Console.WriteLine($"p2.Equals(p3)? {p2.Equals(p3)}");
+            Console.WriteLine($"p2 == p4? {p2==p4}");
+            Console.WriteLine($"p2.Equals(p4)? {p2.Equals(p4)}");
+
+            Console.WriteLine("============");
+
+            Point3D p5 = new Point3D(), p6 = new Point3D(7, 5, 12), p7 = new Point3D(-2, 4, -4), p8 = new Point3D(7, 5, 12);
+
+            Console.WriteLine($"p5 = {p5}");
+            Console.WriteLine($"p6 = {p6}");
+            Console.WriteLine($"p7 = {p7}");
+            Console.WriteLine($"p8 = {p8}");
+
+            Console.WriteLine($"p6 == p7? {p6 == p7}");
+            Console.WriteLine($"p6.Equals(p7)? {p6.Equals(p7)}");
+            Console.WriteLine($"p6 == p8? {p6 == p8}");
+            Console.WriteLine($"p6.Equals(p8)? {p6.Equals(p8)}");
+
+            Console.WriteLine("============");
+            Console.Read();
+        }
+
+       
+    }
+}
---
+++ Csharp Programs/Superheros and Supervillans/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superheros_and_Supervillans
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string NickName { get; set; }
+
+
+        public Person(string x, string y)
+        {
+            Name = x;
+            NickName = y;
+        }
+        public override string ToString()
+        {
+            return $"{Name}: ";
+        }
+        public string PrintGreeting()
+        {
+            return $"Hi, my name is {Name}, you can call me {NickName}.";
+        }
+    }
+    class Superhero : Person
+    {
+        public string RealName { get; set; }
+        public string SuperPower { get; set; }
+
+        public Superhero(string name, string superpower, string realname) : base( name, null )
+
+        {
+            SuperPower = superpower;
+            RealName = realname;
+        }
+        public new string PrintGreeting()
+        {
+            return $"I am {RealName}. When I am {Name}, my super power is {SuperPower}!";
+        }
+    }
+    class SuperVillan: Person
+    {
+        public string Nemesis { get; set; }
+
+        public SuperVillan(string name, string nemesis): base(name, null)
+        {
+            Nemesis = nemesis;
+        }
+
+        public new string PrintGreeting()
+        {
+            return $"I am the {Name}. Have you seen {Nemesis}?";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person one = new Person("William", "Bill");
+            Superhero two = new Superhero("Mr Incredible", "super strength", "Wade Turner");
+            SuperVillan three = new SuperVillan("The Joker", "Batman");
+
+            Console.WriteLine($"{one.ToString()}{one.PrintGreeting()}");
+            Console.WriteLine($"{two.ToString()}{two.PrintGreeting()}");
+            Console.WriteLine($"{three.ToString()}{three.PrintGreeting()}");
+            Console.Read();
+
+        }
+    }
+}
